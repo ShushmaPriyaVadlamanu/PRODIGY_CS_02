3484c6831f661c7d8a867f70fd7c2ace587eefa2
ShushmaPriyaVadlamanu <149227381+ShushmaPriyaVadlamanu@users.noreply.github.com>
2024-06-27 13:33:57 +0530

    Output.py

---
+++ Output.py
@@ -0,0 +1,24 @@
+C:\Users\Sushma\PycharmProjects\PRODIGY_CS_02\.venv\Scripts\python.exe C:\Users\Sushma\PycharmProjects\PRODIGY_CS_02\image_encryption.py 
+Type 'encrypt' to encrypt an image, 'decrypt' to decrypt an image, or 'exit' to quit: encrypt
+Enter the path to the image: C:\Users\Sushma\PycharmProjects\PRODIGY_CS_02\example.png
+Enter the path for the output image: C:\Users\Sushma\PycharmProjects\PRODIGY_CS_02\encrypted_example.png
+Enter an encryption key (integer): 3
+Image encrypted and saved to C:\Users\Sushma\PycharmProjects\PRODIGY_CS_02\encrypted_example.png
+Type 'encrypt' to encrypt an image, 'decrypt' to decrypt an image, or 'exit' to quit: decrypt
+Enter the path to the image: C:\Users\Sushma\PycharmProjects\PRODIGY_CS_02\encrypted_example.png
+Enter the path for the output image: C:\Users\Sushma\PycharmProjects\PRODIGY_CS_02\decrypted_example.png
+Enter an encryption key (integer): 3
+Image decrypted and saved to C:\Users\Sushma\PycharmProjects\PRODIGY_CS_02\decrypted_example.png
+Type 'encrypt' to encrypt an image, 'decrypt' to decrypt an image, or 'exit' to quit: encrypt
+Enter the path to the image: C:\Users\Sushma\PycharmProjects\PRODIGY_CS_02\rose.png
+Enter the path for the output image: C:\Users\Sushma\PycharmProjects\PRODIGY_CS_02\encrypted_rose.png
+Enter an encryption key (integer): 2
+Image encrypted and saved to C:\Users\Sushma\PycharmProjects\PRODIGY_CS_02\encrypted_rose.png
+Type 'encrypt' to encrypt an image, 'decrypt' to decrypt an image, or 'exit' to quit: decrypt
+Enter the path to the image: C:\Users\Sushma\PycharmProjects\PRODIGY_CS_02\encrypted_rose.png
+Enter the path for the output image: C:\Users\Sushma\PycharmProjects\PRODIGY_CS_02\decrypted_rose.png
+Enter an encryption key (integer): 2
+Image decrypted and saved to C:\Users\Sushma\PycharmProjects\PRODIGY_CS_02\decrypted_rose.png
+Type 'encrypt' to encrypt an image, 'decrypt' to decrypt an image, or 'exit' to quit: exit
+
+Process finished with exit code 0
